afa2ddfc7e033c7f7e095f6d456b796e18b86c67
Simone Viglione <zimoviglio51@gmail.com>
2023-04-19 16:58:14 +0200

    milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+function generatePassword($length) {
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-=+;:,.?';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $index = rand(0, strlen($chars) - 1);
+        $password .= $chars[$index];
+    }
+    return $password;
+}
+
+if (isset($_GET['length'])) {
+    $length = (int)$_GET['length'];
+    if ($length > 0) {
+        $password = generatePassword($length);
+        echo "Your password is: $password"; 
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +28,13 @@
     <title> Password generator </title>
 </head>
 <body>
-    
+    <h1> Generatore di Password </h1>
+
+    <form action="index.php" method="get">
+        <label for="lunghezza">Lunghezza password:</label>
+        <input type="number" id="lunghezza" name="length" />
+
+        <button type="submit" value="Genera password"> Genera Password </button>
+    </form>
 </body>
 </html>
